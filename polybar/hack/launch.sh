5035ac05c864a0ccd424e785571f3dfad0a6fbdf
DavidViso <visotskydavid@gmail.com>
2022-03-17 10:14:23 -0400

    rofi pwr menu

--- polybar/hack/launch.sh
+++ polybar/hack/launch.sh
@@ -11,5 +11,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-polybar -q top -c "$DIR"/config.ini &
+polybar -q left -c "$DIR"/config.ini &
 polybar -q bottom -c "$DIR"/config.ini &
+polybar -q right -c "$DIR"/config.ini &
